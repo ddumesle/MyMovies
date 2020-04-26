6c0baffcbf8c11afe70772b770d28606fd0d9174
Dwayne <ddumesle@fau.edu>
2020-04-26 19:33:17 -0400

    Updated MainActivity.

--- app/src/main/java/com/example/mymovies/MainActivity.java
+++ app/src/main/java/com/example/mymovies/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.mymovies;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -19,8 +26,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        checkCredentials();
+        initToolbar();
+        //checkCredentials();
     }
+
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.add) {
+
+        } else {
+            super.onOptionsItemSelected(item);
+        }
+
+        return true;
+    }
+
 
     /**
      * A function that checks whether a user is logged-in

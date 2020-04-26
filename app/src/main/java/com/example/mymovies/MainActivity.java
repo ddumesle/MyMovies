65fedd59c0646ddbcea1449af71a425e7f2c252a
Dwayne <ddumesle@fau.edu>
2020-04-26 16:09:27 -0400

    Splash Activity Layout Completed.

--- app/src/main/java/com/example/mymovies/MainActivity.java
+++ app/src/main/java/com/example/mymovies/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.mymovies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final static int SPLASH = 0;
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseUser currentUser = mAuth.getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkCredentials();
+    }
+
+    /**
+     * A function that checks whether a user is logged-in
+     * with valid credentials. I
+     */
+    private void checkCredentials() {
+        if (currentUser != null) {
+            updateUI();
+        } else {
+            Intent intent = new Intent(this, SplashActivity.class);
+            startActivityForResult(intent, SPLASH);
+        }
+    }
+
+    private void updateUI() {
+        // Recycler View
     }
 }
---
+++ app/src/main/java/com/example/mymovies/RegisterFragment.java
@@ -0,0 +1,6 @@
+package com.example.mymovies;
+
+import androidx.fragment.app.Fragment;
+
+public class RegisterFragment extends Fragment {
+}

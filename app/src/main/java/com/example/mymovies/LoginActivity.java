784514ef995ff221ee64dcff6cf347bd97df12ee
Dwayne <ddumesle@fau.edu>
2020-04-26 18:25:13 -0400

    Updated LoginActivity.

--- app/src/main/java/com/example/mymovies/LoginActivity.java
+++ app/src/main/java/com/example/mymovies/LoginActivity.java
@@ -1,14 +1,64 @@
 package com.example.mymovies;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final TextView emailField = findViewById(R.id.email);
+        final TextView passwordField = findViewById(R.id.password);
+        final Button login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailField.getText().toString();
+                String password = passwordField.getText().toString();
+                login(email, password);
+            }
+        });
+    }
+
+    /**
+     * A function that attempts to sign-in the user via Firebase
+     * authentication.
+     * @param email email address of the user
+     * @param password password of the user
+     */
+    private void login(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Snackbar.make(findViewById(R.id.content),
+                                    "Authentication Failed.",
+                                    Snackbar.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }

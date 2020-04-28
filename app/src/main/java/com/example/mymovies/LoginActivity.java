ecda32212776ee13a28f0e150213a54dab7ef0fd
Dwayne <ddumesle@fau.edu>
2020-04-28 10:49:07 -0400

    Created SearchResultsActivity.

--- app/src/main/java/com/example/mymovies/LoginActivity.java
+++ app/src/main/java/com/example/mymovies/LoginActivity.java
@@ -3,7 +3,6 @@ package com.example.mymovies;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
--- app/src/main/java/com/example/mymovies/SearchResultsActivity.java
+++ app/src/main/java/com/example/mymovies/SearchResultsActivity.java
@@ -1,6 +1,31 @@
 package com.example.mymovies;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class SearchResultsActivity extends AppCompatActivity {
+
+    private final static String API_KEY = "9209cdbe";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+        submitRequest();
+    }
+
+    private void submitRequest() {
+        String query = getQueryString();
+    }
+
+    private String getQueryString() {
+        String result = getIntent().getStringExtra("KEYWORD");
+        String type = getIntent().getStringExtra("TYPE");
+        String year = getIntent().getStringExtra("YEAR");
+
+        return result;
+    }
 }
